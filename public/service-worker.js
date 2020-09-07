14258ed59c54cc07a8b67cb9e9f7e0ada8dc7fca
Darren Behan <darrenbehan@hotmail.com>
2020-09-07 13:44:18 +1000

    setup of install functionality

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,27 @@
-console.log("Hello from service worker!")
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const iconSizes = ["192", "512"];
+const iconFiles = iconSizes.map(
+  (size) => `/assets/images/icons/icon-${size}x${size}.png`
+);
+
+const staticFilesToPreCache = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/manifest.webmanifest",
+  "/style.css"
+].concat(iconFiles);
+
+// install
+self.addEventListener("install", function(evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(staticFilesToPreCache);
+    })
+  );
+
+  self.skipWaiting();
+});
